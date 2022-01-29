04f5167a00a443946dbcf36ad45e778947792d1e
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-01-30 01:37:22 +0300

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -51,7 +51,6 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=[
